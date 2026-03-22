6dc67988ed256a07105cd9ab3fb623ee1c1f5200
German <germancandelario@hotmail.com>
2026-03-22 01:34:05 -0500

    Corrigiendo formato de conexión para Railway

--- TodoParaTuTractoCamion.Infrastructure/DependencyInjection.cs
+++ TodoParaTuTractoCamion.Infrastructure/DependencyInjection.cs
@@ -12,8 +12,18 @@ namespace TodoParaTuTractoCamion.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Si la cadena viene en formato postgres:// (típico de Railway/Heroku)
+            if (connectionString != null && connectionString.StartsWith("postgres://"))
+            {
+                var uri = new Uri(connectionString);
+                var userInfo = uri.UserInfo.Split(':');
+                connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
+            }
+
             services.AddDbContext<TractoCamionDbContext>(options =>
-                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(connectionString));
 
             services.AddScoped<IProductoRepository, ProductoRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
