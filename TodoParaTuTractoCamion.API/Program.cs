1630a909c59c8192f29f192a5d69269cf54e8095
German <germancandelario@hotmail.com>
2026-03-25 22:51:45 -0500

    Enable Swagger UI in Production

--- TodoParaTuTractoCamion.API/Program.cs
+++ TodoParaTuTractoCamion.API/Program.cs
@@ -37,10 +37,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseHttpsRedirection();
 }
 
